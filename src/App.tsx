a8a16eb1f64c1ac0ff4ff436ac8b8b11be5e9f50
Maxim <markushitstep@gmail.com>
2022-02-22 16:16:35 +0200

    create component UserItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Card, CardVariant } from './components/card';
+import UserList  from './components/UserList';
+import { IUser } from './types/types';
 
 const App = () => {
+  const users: IUser[] = [
+    {id: 1, name: "Max", email: "Maximus@gmail.com", address: {city: "Zp", street: "Reliefnaya", zipcode: "06213"}},
+    {id: 1, name: "Leo", email: "Testing@gmail.com", address: {city: "UK", street: "Lenina", zipcode: "02313"}}
+  ] 
+
   return (
     <div>
         <Card onClick={(num) => console.log('click', num) } variant={CardVariant.primary} width='200px' height='200px'>
             <button>Button</button>
         </Card>
+        <UserList users={users}/>
     </div>
   )
 }
