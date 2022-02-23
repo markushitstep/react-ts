5046296ad87812b97ba7900ac51c8e235f827d61
Maxim <markushitstep@gmail.com>
2022-02-23 09:31:46 +0200

    typing useRef hook

--- src/components/EventsExample.tsx
+++ src/components/EventsExample.tsx
@@ -1,17 +1,20 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useRef, useState } from 'react';
 import { ITodo } from '../types/types';
 
 
 export const EventsExample: FC = () => {
     const [value, setValue] = useState<string>('');
     const [isDrag, setIsDrag] = useState<boolean>(false);
+    const inputRef = useRef<HTMLInputElement>(null)       
+
 
     const changeHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
         setValue(e.target.value);
     }
 
     const clickHandler = (e: React.MouseEvent<HTMLButtonElement>): void => {
-        console.log(value)
+        console.log(inputRef.current?.value)
+
     }
 
     const dragHandler = (e: React.DragEvent<HTMLDivElement>): void => {
@@ -37,7 +40,8 @@ export const EventsExample: FC = () => {
 
     return (
         <div>
-            <input value={value} onChange={changeHandler} type="text" />
+            <input value={value} onChange={changeHandler} type="text" placeholder="Управляемый"/>
+            <input ref={inputRef} type="text" placeholder="Неуправляемый"/>
             <button onClick={clickHandler}>click</button>
             <div onDrag={dragHandler} draggable style={{ width: '200px', height: "200px", backgroundColor: "red", marginBottom: '40px' }}></div>
             <div
